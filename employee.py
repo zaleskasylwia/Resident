df8dedb75cc9757aa0de0698786f8a135d28dc07
sylwia <zaleska.syl@gmail.com>
2017-08-07 08:42:53 +0200

    Add str and calculate salary method

--- employee.py
+++ employee.py
@@ -1,8 +1,17 @@
 from .person import Person
 
+
 class Employee(Person):
     def __init__(self, first_name, last_name, position, year_of_emp, rate_per_hour):
         super().__init__(first_name, last_name)
         self.position = position
         self.year_of_emp = year_of_empk
         self.rate_per_hour = rate_per_hour
+
+    def __str__(self):
+        return "{} {} {} {} {}".format(self.first_name, self.last_name, self.position, self.year_of_emp, self.rate_per_hour)
+
+    def calucalte_salary(self, working_hours):
+        if working_hours.isdigit():
+            return int(self.rate_per_hour * working_hours)
+        return None
