5e5cb84b67b982fc6ee0020691023fa9a33a5147
sylwia <zaleska.syl@gmail.com>
2017-08-07 08:36:48 +0200

    Add str and calucalte rent method

--- resident.py
+++ resident.py
@@ -7,3 +7,9 @@ class Resident(Person):
         self.flat_nr = flat_nr
         self.flat_area = flat_area
         self.price_per_metr = price_per_metr
+
+    def __str__(self):
+        return "{} {} {} {} {}".format(self.first_name, self.last_name, self.flat_nr, self.flat_area, self.price_per_metr)
+
+    def calculate_rent(self):
+        return int(self.flat_area * self.price_per_metr)
